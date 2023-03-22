b30edd14170e7d44203b146a22df8a340279e3b5
reshmalb <reshma.lb27@gmail.com>
2023-03-22 18:42:27 +0530

    Food App Header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Components/Layout/Header"; 
+
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React ,{Fragment}from 'react';
+import mealsImage from '../../assets/FoodHeader.png'
+import classes from './Header.module.css'
+const Header=props=>{ 
+   return(
+    <Fragment>
+        <header className={classes.header}>
+         
+           <button>Show Cart</button> 
+        </header>
+            <div>
+                <img src={mealsImage} alt="delicious food"></img>
+            
+            </div>
+    </Fragment> 
+   )
+       
+}
+export default Header;
